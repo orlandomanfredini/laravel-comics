2b6d2d8ce7aa4dc342d858daf48132635cafd7bc
orlando manfredini <orlando.manfre1@gmail.com>
2024-05-22 15:05:03 +0200

    ADD: comics.php

--- resources/views/mainHome.blade.php
+++ resources/views/mainHome.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="container">
-    <h1>BENVENUT* HOME LARAVEL</h1>
-    <P>
+    <h1 class="text-center">BENVENUT*  NELLA HOME COMICS LARAVEL</h1>
+    <P class="text-center mt-4">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Error sit sint omnis earum possimus perferendis! Aperiam similique esse eveniet dicta nobis blanditiis velit eligendi, eum recusandae, ipsa distinctio quidem hic!
     </P>
 </div>
